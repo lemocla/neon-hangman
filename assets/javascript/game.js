19245c8e736e056ab5a3238f6b53013c780dad02
lemocla <lemocla@icloud.com>
2021-04-21 21:39:08 +0000

    Add mobile volume svg & add test buttons to display dynamic content

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    /* test show word - start */
+    $('#start').on("click", function () {
+        $('#start').addClass("hide");
+        $('.word').removeClass("hide");
+    });
+
+    /* test show game win message */
+    $('#test-win').on("click", function () {
+        $('.keyboard-container').addClass("hide");
+        $('#game-win').removeClass("hide");
+    });
+
+    /* test show game over message */
+    $('#test-over').on("click", function () {
+        $('.flex-container').addClass("hide");
+        $('#game-over').removeClass("hide");
+    });
+
+    /* test back to start */
+    $('#backtostart').on("click", function () {
+        $('.keyboard-container').removeClass("hide");
+        $('#game-win').addClass("hide");
+
+        $('.flex-container').removeClass("hide");
+        $('#game-over').addClass("hide");
+
+        $('#start').removeClass("hide");
+        $('.word').addClass("hide");
+    });
+})
