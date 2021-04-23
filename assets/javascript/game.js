a5f8a9af16c0138d0b0ebb1f20076ab7b75b8e00
lemocla <lemocla@icloud.com>
2021-04-23 09:18:34 +0000

    Add wordsAPI to generate and display random words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,15 +8,53 @@ $(document).ready(function () {
         $('#hint').removeClass("hide");
     });
 
-    //Split word into an array
+
     let word = "test";
-    splitWord = word.split("");
-    //Display word
-    $.each(splitWord, function (index, value) {
-        $(".word").append(
-            `<div class="letter-box" id="${index}"></div>`
-        )
-    });
+
+    function displayWord(word) {
+        //Split word into an array
+        splitWord = word.split("");
+        //Display word
+        $.each(splitWord, function (index, value) {
+            $(".word").append(
+                `<div class="letter-box" id="${index}"></div>`
+            )
+        });
+    }
+
+
+    //*//Generate random word - wordsAPI
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=3&lettersMax=6&limit=5&page=1&frequencyMin=6",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "b1b8b66d72mshfbfc05708a9c0e5p10ad1bjsn0f9cbb550588",
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+        },
+        "dataType": "json",
+    };
+
+    $.ajax(settings).done(function (dataType) {
+            console.log("word " + dataType.word);
+            displayWord(dataType.word);
+
+            console.log(dataType);
+        })
+        .fail(
+            function (xhr) {
+                var errorMessage = xhr.status + ': ' + xhr.statusText
+                console.log('Error - ' + errorMessage);
+            }
+
+        );
+
+
+
+
+
+
 
     /* test show game win message */
     $('#test-win').on("click", function () {
