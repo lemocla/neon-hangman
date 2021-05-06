4464864e267665dddfe77c7c28cf8244ef743b1a
lemocla <lemocla@icloud.com>
2021-05-06 07:57:40 +0000

    Fix closing bug & style for contact form & modals

--- assets/javascript/emailjs.js
+++ assets/javascript/emailjs.js
@@ -4,17 +4,16 @@ $(document).ready(function () {
     //https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_submit_prevent
     $("#form").submit(function (event) {
         event.preventDefault();
-        const serviceID = 'default_service';
-        const templateID = 'template_90dzp4a';
+        const serviceID = "default_service";
+        const templateID = "template_90dzp4a";
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
-                console.log('Sent!');
-                $('#modal-thankyou').removeClass('hide');
+                $('#modal-thankyou').removeClass("hide");
                 $("#form")[0].reset();
             }, () => {
-                console.log('error');
-                $('#modal-error').removeClass('hide');
+                $('#modal-error').removeClass("hide");
                 $("#form")[0].reset();
             });
+        $("#modal-form").addClass("hide");
     });
 });
